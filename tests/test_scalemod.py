a7451e117b8bba5d3e0235173c32628eaa19a143
Eric Brauer <eric.m.brauer@gmail.com>
2019-08-07 11:24:29 -0400

    various housekeeping changes

--- tests/test_scalemod.py
+++ tests/test_scalemod.py
@@ -1,38 +1,37 @@
 #!/usr/bin/python3
 import sys
 import unittest as unittest
-sys.path.append('../')
+sys.path.append('../scale_tool')
 from scale_mod import Scale, NoRootError, BadRootError, BadScaleError
 
 
 class Test(unittest.TestCase):
 
     def test_chromatic_scale(self):
-        s = Scale(root = 'c', scale_name = 'major')
+        s = Scale(root='c', scale_name='major')
         self.assertEqual(s.get_chromatic_scale(), ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#',
                      'A', 'A#', 'B', 'C'])
 
     def test_c_maj(self):
-        s = Scale(root = 'c', scale_name = 'major')
+        s = Scale(root='c', scale_name='major')
         self.assertEqual(s.get_scale_notes(), ['C', 'D', 'E', 'F', 'G', 'A', 'B', 'C'])
 
     def test_roots(self):
         key_list = ['C', 'D', 'E', 'F', 'G', 'A', 'B', 'D#', 'C']
         for key in key_list:
-            s = Scale(root = key, scale_name = 'major')
+            s = Scale(root=key, scale_name='major')
             self.assertEqual(s.root, key)
-
 
     @unittest.skip('back to drawing board')
     def test_intervals(self):
         key_list = ['C', 'D', 'E', 'F', 'G', 'A', 'B', 'C']
-        s = Scale(root = 'c', scale_name = 'major')
+        s = Scale(root='c', scale_name='major')
         for key in key_list:
             self.assertEqual(s.get_interval(key_list.index(key)+1), key)
 
     # @unittest.skip('check that previsou test succeeds')
     def test_bad_interval(self):
-        s = Scale(root = 'c', scale_name = 'major')
+        s = Scale(root='c', scale_name='major')
         with self.assertRaises(AssertionError):
             s.get_interval(9)
 
@@ -43,7 +42,7 @@ class Test(unittest.TestCase):
 
     def test_bad_scale(self):
         with self.assertRaises(BadScaleError):
-            s = Scale(root='c', scale_name = 'garbage')
+            s = Scale(root='c', scale_name='garbage')
 
     def test_minor(self):
         correct = ['C', 'D', 'D#', 'F', 'G', 'G#', 'A#', 'C']
@@ -63,7 +62,7 @@ class Test(unittest.TestCase):
     def test_flat_scale(self):
         correct = ['D', 'Eb', 'F', 'G', 'Ab', 'Bb', 'C', 'D']
         s = Scale(root='d', scale_name='LOCRIAN')
-        self.assertEquals(s.get_flat_scale(), correct)
+        self.assertEqual(s.get_flat_scale(), correct)
 
     @unittest.skip('this is not implemented yet')
     def test_finer_adjustment(self):
